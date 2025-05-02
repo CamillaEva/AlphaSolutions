54ccd0474bac36703a4ee24264bb4f1983917e15
KasperPersson <kape0010@stud.kea.dk>
2025-05-02 13:20:51 +0200

    added controller class

---
+++ src/main/java/com/example/alphasolutions/controller/Controller.java
@@ -0,0 +1,18 @@
+package com.example.alphasolutions.controller;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+
+    //____________________________________CREATE METHODS____________________________________
+
+
+    //____________________________________READ METHODS______________________________________
+
+
+    //____________________________________UPDATE METHODS____________________________________
+
+
+    //____________________________________DELETE METHODS____________________________________
+
+}
