eee841fe4a14f2fd57e63645ee38d32a31d8db1a
laerkeloenborg <lalo0001@stud.kea.dk>
2025-05-22 11:45:17 +0200

    integrationstest setup

---
+++ src/test/java/com/example/alphasolutions/repository/TaskRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.example.alphasolutions.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+        scripts = {"classpath:h2init.sql"}
+)
+class TaskRepositoryTest {
+
+    @Test
+    void assignEmpToTask() {
+    }
+
+    @Test
+    void showAssignedEmpTask() {
+    }
+
+    @Test
+    void createTask() {
+    }
+
+    @Test
+    void readAllTask() {
+    }
+
+    @Test
+    void readTaskByID() {
+    }
+
+    @Test
+    void readMyTasks() {
+    }
+
+    @Test
+    void readTotalTimeEstimateForProject() {
+    }
+
+    @Test
+    void readTotalUsedTimeForProject() {
+    }
+
+    @Test
+    void updateTask() {
+    }
+
+    @Test
+    void updateUsedTime() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+}
