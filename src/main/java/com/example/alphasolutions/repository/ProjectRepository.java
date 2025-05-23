9e333c7607b9b7dd9cab156460ff70acc6ae4456
KasperPersson <kape0010@stud.kea.dk>
2025-05-23 10:24:08 +0200

    method call had wrong camelcase

--- src/main/java/com/example/alphasolutions/repository/ProjectRepository.java
+++ src/main/java/com/example/alphasolutions/repository/ProjectRepository.java
@@ -65,7 +65,7 @@ public class ProjectRepository {
         String sql = "SELECT P.PROJECTID, P.NAME, P.DESCRIPTION, P.STARTDATE, P.ENDDATE, SP.SUBPROJECTID AS SPID, SP.NAME AS SPNAME, SP.DESCRIPTION AS SPDESCRIPTION, SP.STARTDATE AS SPSTARTDATE, SP.ENDDATE AS SPENDDATE/*, SP.TIMEEST AS SPTIMEEST*/ " +
                 "FROM PROJECT P " +
                 "LEFT JOIN SUBPROJECT SP ON P.PROJECTID = SP.PROJECTID WHERE P.PROJECTID = ?";
-        return projectMapper.ProjectWithSubProjects(jdbcTemplate.queryForList(sql, projectID)).get(0);
+        return projectMapper.projectWithSubProjects(jdbcTemplate.queryForList(sql, projectID)).get(0);
     }
 
     public int readTotalTimeEstimateForProject(int projectID) {
